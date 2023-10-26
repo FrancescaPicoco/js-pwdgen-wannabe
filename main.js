9d1cff63473df31188970509927a1f09ca957ebb
FrancescaPicoco <francesca.picoco33@gmail.com>
2023-10-26 11:04:55 +0200

    ...

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ const nomeUtente = prompt( "Inserisci il tuo nome", "Francesca");
 const cognomeUtente = prompt( "Inserisci il tuo cognome", "Picoco");
 const coloreUtente = prompt( "Inserisci il tuo colore", "carta da zucchero");
 const passWord = "Francesca" + "Picoco" + "carta da zucchero";
+//const passWord = "nomeUtente" + "cognomeUtente" + "coloreUtente"
+console.log(nomeUtente);
+console.log(cognomeUtente);
+console.log(coloreUtente);
 
 document.getElementById("password").innerHTML = `Password: ${passWord} #23`;
